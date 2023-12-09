dcf4e9b85b74cf207085f801d3fd40c0556f4b5c
GazVanc <gdavies2736@gmail.com>
2023-12-09 16:57:52 +0000

    updated code to use data from api inside todays weather card

--- assets/script.js
+++ assets/script.js
@@ -8,22 +8,105 @@ var apiKey = "2bbc1080b504d6f4e2cad6a27f5acc94"
 //var city = London
 
 
-//On click of search button, need event listener
+//On click of search button, use event listener
+//prevent input from refreshing 
+//create variable for city entered using inputs value
+//use city variable in api request
+//request data using api + city (convert units to metric in request)
 
 document.getElementById("search-button").addEventListener("click",function(event){
     event.preventDefault();
     document.getElementById("search-input").value;
     var city = document.getElementById("search-input").value;
-    var URL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    var URL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
 fetch(URL)
 .then(function(response){
     return response.json();
 }).then(function(data) {
     console.log(data);
+    displayCurrentWeather (data);
+    todaysdate();
+    //document.getElementById("citymainheader").innerText = data[1].name;
 })
 })
 
-//units=metric
+//Display date
+
+function todaysdate (){
+var todaysdate = dayjs().format('LL');
+console.log(todaysdate);
+document.getElementById("todaysdate").innerText = todaysdate;
+}
+
+//Add city name
+function displayCurrentWeather (data) {
+var cityName = data.name;
+document.getElementById("citymainheader").innerText = cityName;
+var temperature = data.main.temp;
+document.getElementById("temperature").innerText = "Temperature: " + temperature + "° Celsius";
+var wind = data.wind.speed;
+document.getElementById("wind").innerText = "Wind: " + wind + " kmph";
+var humidity = data.main.humidity;
+document.getElementById("humidity").innerText = "Humidity: " + humidity + "%";
+}
+
+
+
+//Add current weather to main weather section on page
+
+//document.getElementById("citymainheader").innerText = data.main
+//var citymain = 
+
+
+//request 5 day forecast
+//In order to get 5 day forecast:
+//1. Need to use Geocoder API to convert city to exact geographical coordinates
+//2.Use the co-ordinates to create new api request for 5 day forecast
+
+
+//to convert city to exact geographical coordinates
+//http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
+
+document.getElementById("search-button").addEventListener("click",function(event){
+    event.preventDefault();
+    document.getElementById("search-input").value;
+    var city = document.getElementById("search-input").value;
+var latandlongrequestURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5" + "&appid=" + apiKey;
+fetch(latandlongrequestURL)
+.then(function(response){
+    return response.json();
+}).then (function(data){
+    //console.log(data);
+    //console.log(data[0].lat);
+    var latitude = data[0].lat;
+    console.log(latitude);
+   var longitude = data[0].lon;
+   console.log(longitude);
+   var fivedayweatherURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
+   fetch(fivedayweatherURL)
+   .then(function(response){
+    return response.json();
+}).then (function(data){
+    console.log(data);
+})
+})
+})
+
+
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+
+
+//need to use Geocoder API to convert city to exact geographical coordinates
+
+//http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
+
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+
+
+
+
 
 
 
